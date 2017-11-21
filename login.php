4eaf49ad33570fba1476758bc764214e4b0951fd
Teet Triisa <31885404+TTriisa@users.noreply.github.com>
2017-11-21 14:31:03 +0200

    login template add
    asd

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="main.css">
+	<title>NaliLeht</title>
+</head>
+<body>
+	<div class="wrap">
+		<?php require("header.php")?>
+		siia loginid vvvvv
+	</div>
+</body>
+</html>
+
+
