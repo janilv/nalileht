384565af6dd23621dd13d8449a2d921454056ece
Teet Triisa <31885404+TTriisa@users.noreply.github.com>
2017-11-21 13:48:34 +0200

    teinepush
    asd

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="main.css">
 	<title>NaliLeht</title>
 </head>
 <body>
-	<div>
-		Hello world!
+	<div class="meemid">
+		<div class="meme">
+			
+		</div>
 	</div>
 	
 </body>
