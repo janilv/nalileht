cb97d6174c6543f9bff2cd83813a34616ffb6cf1
Simone Niinemägi <31885412+niinsimo@users.noreply.github.com>
2017-12-05 15:38:14 +0200

    logo
    logo

--- header.php
+++ header.php
@@ -1,19 +1,14 @@
 <?php
+
 ?>
 <div class="navbar-inverse navbar-fixed-top navbar">
 	<div class="container">
 		<div class="float-left">
-			<a class="text" href="index.php">Kodu</a>
+			<a href="index.php"><img src="lollog.png" border="0" alt="meievaikelogo.png" /></a>
 		</div>
-		<?= isset($_SESSION["userId"]) ? '
 		<div class="float-right">
-			<a class="text" href="login.php?logout">Logi välja ('.$_SESSION["userlogin"].')</a>
+			<a class="text" href="login.php">Logi sisse</a>
 		</div>
-		':'
-		<div class="float-right">
-			<a class="text" href="login.php?">Logi sisse</a>
-		</div>'
-		?>
 		<?= isset($_SESSION["userId"]) ? '
 		<div class="float-middle">
 			<a class="text" href="newmeme.php">Lisa meme</a>
