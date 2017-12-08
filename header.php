bb4416e70896520d587876a7dbb2caf087a4cff9
Teet Triisa <31885404+TTriisa@users.noreply.github.com>
2017-12-08 14:03:54 +0200

    login/out fix

--- header.php
+++ header.php
@@ -6,9 +6,15 @@
 		<div class="float-left">
 			<a href="index.php"><img src="lollog.png" border="0" alt="meievaikelogo.png" /></a>
 		</div>
+		<?= isset($_SESSION["userId"]) ? '
 		<div class="float-right">
-			<a class="text" href="login.php">Logi sisse</a>
+			<a class="text" href="login.php?logout">Logi välja ('.$_SESSION["userlogin"].')</a>
 		</div>
+		':'
+		<div class="float-right">
+			<a class="text" href="login.php?">Logi sisse</a>
+		</div>'
+		?>
 		<?= isset($_SESSION["userId"]) ? '
 		<div class="float-middle">
 			<a class="text" href="newmeme.php">Lisa meme</a>
